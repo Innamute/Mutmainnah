92ab5e97f81a18588244510d05a80bf9caa36e0f
Innamute <147324079+Innamute@users.noreply.github.com>
2023-10-09 19:23:15 +0800

    Add files via upload

---
+++ d1.java
@@ -0,0 +1,28 @@
+package mutmainna;
+
+public class d1 {
+
+    public static void main(String[] args) {
+        String nama = "mutmainna";
+        long tahunLahir = 2002;
+        short tanggalLahir = 14;
+        byte bulan = 6;
+        int umur = 20;
+        double tinggiBadan = 150;
+        float beratBadan = 55.0f;
+        char ukuranCelana = 'L';
+        boolean statusMenikah = false;
+
+        System.out.println("nama :" + nama);
+        System.out.println("tahunLahir :" + tahunLahir);
+        System.out.println("tanggalLahir :" + tanggalLahir);
+        System.out.println("bulan :" + bulan);
+        System.out.println("umur :" + umur);
+        System.out.println("tinggiBadan :" + tinggiBadan);
+        System.out.println("beratBadan :" + beratBadan);
+        System.out.println("ukuranCelana :" + ukuranCelana);
+        System.out.println("statusMenikah :" + statusMenikah);
+
+    }
+
+}
